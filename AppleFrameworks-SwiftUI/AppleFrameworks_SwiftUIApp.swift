0586ef363322ae291e947e456a1a55aafc356444
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-06-13 19:07:51 -0400

    Initial Commit

---
+++ AppleFrameworks-SwiftUI/AppleFrameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_SwiftUIApp.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Shilpa Joy on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameworks-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Shilpa Joy on 2024-06-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
