7490123863556414b6e2178c5ddf281710055f22
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-06-14 16:27:14 -0400

    Initial commit

---
+++ AppleFrameworks-SwiftUI/Framework.swift
@@ -0,0 +1,8 @@
+//
+//  Framework.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Shilpa Joy on 2024-06-13.
+//
+
+import Foundation
---
+++ AppleFrameworks-SwiftUI/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Shilpa Joy on 2024-06-14.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
